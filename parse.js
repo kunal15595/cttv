d5d9f6bea27c8685c2d10df7a1f0eb45f90e6a54
Kunal Khandelwal <kunal15595@gmail.com>
2016-03-19 12:28:12 +0530

    initial working

---
+++ parse.js
@@ -0,0 +1,247 @@
+Papa.SCRIPT_PATH = "./papaparse.js";
+
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", initParse);
+
+// remote
+// oReq.open("GET", "http://rawgit.com/kunal15595/cttv/gh-pages/gwas_catalog_v1.0.1-associations_e84_r2016-03-13.tsv");
+
+// localhost
+oReq.open("GET", "http://localhost/cttv/gwas_catalog_v1.0.1-associations_e84_r2016-03-13.tsv");
+
+oReq.send();
+
+
+function initParse() {
+    Papa.parse(this.responseText, {
+        header: true,
+        delimiter: "\t",
+        worker: true,
+        dynamicTyping: true,
+        step: function(row, parser) {
+
+            if (Math.random() < 0.95)
+                return;
+
+            var mappedTraits = row.data[0]['MAPPED_TRAIT'].split(',');
+
+            if (mappedTraits.length > 8 || mappedTraits.length < 2)
+                return;
+
+            // console.log(row.data[0]['INTERGENIC_ENSEMBL'], row.data[0]['SNP_GENE_IDS_ENSEMBL'], row.data[0]['ENSEMBL_UPSTREAM_GENE_ID']);
+            // console.log(row.data[0]['INTERGENIC_ENTREZ'], row.data[0]['SNP_GENE_IDS_ENTREZ'], row.data[0]['ENTREZ_UPSTREAM_GENE_ID']);
+            // console.log("Row:", row.data[0]['STRONGEST SNP-RISK ALLELE']);
+            // console.log(row.data[0]['CHR_ID'], row.data[0]['CHR_POS'],  row.data[0]['REGION']);
+            // console.log(row.data[0]['CHR_POS'] + ': :' + row.data[0]['CHR_ID']);
+
+            var linkSource, linkTarget, linkGene;
+
+            if (!traits.hasOwnProperty(row.data[0]['DISEASE/TRAIT'])) {
+                traits[row.data[0]['DISEASE/TRAIT']] = nodeId;
+                linkSource = nodeId;
+                nodes.push({
+                    "type": "trait",
+                    "id": nodeId++,
+                    "label": "Trait: " + row.data[0]['DISEASE/TRAIT'],
+                    "class": "node trait",
+                    "r": radius - .75,
+                    "charge": -100,
+                    "friction": 0.3,
+                    "chargeDistance": 20,
+					"gene2genes": []
+                });
+
+
+            } else {
+                linkSource = traits[row.data[0]['DISEASE/TRAIT']];
+            }
+
+            if (!targets.hasOwnProperty(row.data[0]['SNP_ID_CURRENT'])) {
+                targets[row.data[0]['SNP_ID_CURRENT']] = nodeId;
+                linkTarget = nodeId;
+                nodes.push({
+                    "type": "target",
+                    "id": nodeId++,
+                    "label": "Target: " + row.data[0]['SNP_ID_CURRENT'],
+                    "class": "node target",
+                    "r": radius - .75,
+                    "charge": -100,
+                    "friction": 0.3,
+                    "chargeDistance": 20,
+					"gene2genes": []
+                });
+            } else {
+                linkTarget = targets[row.data[0]['SNP_ID_CURRENT']];
+            }
+
+            var nodesInStudy = [];
+
+            for (var i = mappedTraits.length - 1; i >= 0; i--) {
+                if (!genes.hasOwnProperty(mappedTraits[i])) {
+                    genes[mappedTraits[i]] = nodeId;
+                    linkGene = nodeId;
+                    nodes.push({
+                        "type": "gene",
+                        "id": nodeId++,
+                        "label": mappedTraits[i],
+                        "r": 2,
+                        "region": mappedTraits[i],
+                        "class": "node gene",
+                        "charge": -70,
+                        "friction": 0.9,
+                        "chargeDistance": 50,
+						"gene2genes": []
+                    });
+                } else {
+                    linkGene = genes[mappedTraits[i]];
+                }
+                nodesInStudy.push(linkGene);
+
+            }
+            nodesInStudy.sort();
+
+            if (nodesInStudy.length > 1) {
+                var connectingNodeId;
+
+                if (!gene2genes.hasOwnProperty(nodesInStudy)) {
+                    gene2genes[nodesInStudy] = nodeId;
+                    connectingNodeId = nodeId;
+
+                    nodes.push({
+                        "type": "gene2gene",
+                        "id": nodeId++,
+                        "label": "Study: " + row.data[0]['STUDY'],
+                        "region": mappedTraits[i],
+                        "class": "node gene2gene",
+                        "r": Math.log(1 + row.data[0]['PVALUE_MLOG']),
+                        "charge": -100,
+                        "friction": 0.02,
+                        "chargeDistance": 50,
+						"genes": [],
+						"traits": [],
+						"targets": []
+                    });
+                    for (var i = nodesInStudy.length - 1; i >= 0; i--) {
+                        links.push({
+                            "source": connectingNodeId,
+                            "target": nodesInStudy[i],
+                            "class": "link gg",
+                            "linkStrength": 1,
+                            "linkDistance": 5
+                        });
+						nodes[nodesInStudy[i]].gene2genes.push(nodes[connectingNodeId]);
+						nodes[connectingNodeId].genes.push(nodes[nodesInStudy[i]]);
+                    }
+                } else {
+                    connectingNodeId = gene2genes[nodesInStudy];
+                }
+
+                links.push({
+                    "source": linkSource,
+                    "target": connectingNodeId,
+                    "class": "link sg",
+                    "linkStrength": 0.2,
+                    "linkDistance": 50
+                });
+                links.push({
+                    "source": connectingNodeId,
+                    "target": linkTarget,
+                    "class": "link gt",
+                    "linkStrength": 0.2,
+                    "linkDistance": 50
+                });
+				nodes[linkSource].gene2genes.push(nodes[connectingNodeId]);
+				nodes[linkTarget].gene2genes.push(nodes[connectingNodeId]);
+
+				nodes[connectingNodeId].traits.push(nodes[linkSource]);
+				nodes[connectingNodeId].targets.push(nodes[linkTarget]);
+
+
+            }
+
+        },
+        complete: function() {
+            console.log("All done!");
+            // console.log(links);
+            viz();
+        }
+    });
+
+
+}
+
+/*
+
+Sample Data (One entry of database) :
+
+	95% CI (TEXT): "[1.23-1.73]"
+	CHR_ID: 21
+	CHR_POS: 39008323
+	CNV: "N"
+	CONTEXT: "intron_variant"
+	DATE: "07-Dec-2006"
+	DATE ADDED TO CATALOG: "25-Nov-2008"
+	DISEASE/TRAIT: "Nicotine dependence"
+	ENSEMBL_DOWNSTREAM_GENE_DISTANCE: ""
+	ENSEMBL_DOWNSTREAM_GENE_ID: ""
+	ENSEMBL_MAPPED_GENE: "AF064858.11"
+	ENSEMBL_UPSTREAM_GENE_DISTANCE: ""
+	ENSEMBL_UPSTREAM_GENE_ID: ""
+	ENTREZ_DOWNSTREAM_GENE_DISTANCE: ""
+	ENTREZ_DOWNSTREAM_GENE_ID: ""
+	ENTREZ_MAPPED_GENE: "LOC102724740"
+	ENTREZ_UPSTREAM_GENE_DISTANCE: ""
+	ENTREZ_UPSTREAM_GENE_ID: ""
+	FIRST AUTHOR: "Bierut LJ"
+	INITIAL SAMPLE DESCRIPTION: "482 European ancestry cases, 466 European ancestry controls"
+	INTERGENIC_ENSEMBL: 0
+	INTERGENIC_ENTREZ: 0
+	JOURNAL: "Hum Mol Genet"
+	LINK: "http://europepmc.org/abstract/MED/17158188"
+	MAPPED_TRAIT: "nicotine dependence"
+	MAPPED_TRAIT_URI: "http://www.ebi.ac.uk/efo/EFO_0003768"
+	MERGED: 0
+	OR or BETA: 1.46
+	P-VALUE: 0.000002
+	P-VALUE (TEXT): ""
+	PLATFORM [SNPS PASSING QC]: "Perlegen [2.4 million] (pooled)"
+	PUBMEDID: 17158188
+	PVALUE_MLOG: 5.698970004336019
+	REGION: "21q22.2"
+	REPLICATION SAMPLE DESCRIPTION: "568 European ancestry cases, 413 European ancestry controls"
+	REPORTED GENE(S): "NR"
+	RISK ALLELE FREQUENCY: 0.4
+	SNPS: "rs2836823"
+	SNP_GENE_IDS_ENSEMBL: "ENSG00000237721"
+	SNP_GENE_IDS_ENTREZ: 102724740
+	SNP_ID_CURRENT: 2836823
+	STRONGEST SNP-RISK ALLELE: "rs2836823-T"
+	STUDY: "Novel genes identified in a high-density genome wide association study for nicotine dependence."
+
+	--------------------------------------
+
+	CHR_ID: 21
+	CHR_POS: 39008323
+	CNV: "N"
+	CONTEXT: "intron_variant"
+	DISEASE/TRAIT: "Nicotine dependence"
+	ENSEMBL_MAPPED_GENE: "AF064858.11"
+	ENTREZ_MAPPED_GENE: "LOC102724740"
+	INTERGENIC_ENSEMBL: 0
+	INTERGENIC_ENTREZ: 0
+	MAPPED_TRAIT: "nicotine dependence"
+	MERGED: 0
+	OR or BETA: 1.46
+	P-VALUE: 0.000002
+	P-VALUE (TEXT): ""
+	PVALUE_MLOG: 5.698970004336019
+	REGION: "21q22.2"
+	REPORTED GENE(S): "NR"
+	RISK ALLELE FREQUENCY: 0.4
+	SNPS: "rs2836823"
+	SNP_GENE_IDS_ENSEMBL: "ENSG00000237721"
+	SNP_GENE_IDS_ENTREZ: 102724740
+	SNP_ID_CURRENT: 2836823
+	STRONGEST SNP-RISK ALLELE: "rs2836823-T"
+*/
---
+++ viz.js
@@ -0,0 +1,281 @@
+var dat = {},
+    nodes = [],
+    links = [];
+var traits = {},
+    targets = {},
+    genes = {},
+    gene2genes = {};
+var traitId = 0,
+    targetId = 0,
+    nodeId = 0;
+
+var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+    height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+    radius = 3;
+
+width *= 0.95;
+height *= 0.95;
+
+var opacity = 0.1;
+
+var focusMode = false,
+    focusedNode;
+
+var clickedOnce = false;
+var timer;
+
+var force = d3.layout.force()
+    .gravity(.05)
+    .friction(0.3)
+    .linkStrength(0.1)
+    .size([width, height])
+    .linkDistance(50)
+    .on("tick", tick);
+
+var drag = force.drag()
+    .on("dragstart", dragstart);
+
+var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-5, 20])
+    .html(function(d) {
+        return '<span class="' + d.class + '">' + d.label + '</span>';
+    });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("fill", "black")
+    .call(tip);
+
+var link = svg.selectAll(".link"),
+    node = svg.selectAll(".node");
+
+function viz() {
+    var graph = {
+        "nodes": nodes,
+        "links": links
+    };
+
+    force
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .linkStrength(function(link) {
+            return link.linkStrength;
+        })
+        .linkDistance(function(link) {
+            return link.linkDistance;
+        })
+        .charge(function(node) {
+            return node.charge;
+        })
+        .start();
+
+    link = link.data(graph.links)
+        .enter().append("line")
+        .attr("class", function(link) {
+            return link.class;
+        });
+
+    node = node.data(graph.nodes)
+        .enter().append("circle")
+        .attr("class", function(node) {
+            return node.class;
+        })
+        .attr("r", function(node) {
+            return node.r;
+        })
+        .attr("charge", function(node) {
+            return node.charge;
+        })
+        .attr("chargeDistance", function(node) {
+            return node.chargeDistance;
+        })
+        .attr("friction", function(node) {
+            return node.friction;
+        })
+        .attr("size", function(node) {
+            return node.size;
+        })
+        .on('mouseover', function(node) {
+            tip.show(node);
+            if (focusMode)
+                return;
+            fade(node, opacity);
+        })
+        .on('mouseout', function(node) {
+            tip.hide(node);
+            if (focusMode)
+                return;
+            unfade(node, opacity);
+        })
+		.on('dblclick', dblclickEvent)
+        .on('click', function(node) {
+			if (timer) {
+				clearTimeout(timer);
+			}
+    		timer = setTimeout(function() {
+				if (!focusMode) {
+					fade(node, 0);
+					focusMode = true;
+					focusedNode = node;
+				} else {
+					unfade(focusedNode, 0);
+					focusMode = false;
+					focusedNode = null;
+					console.log("focus true");
+				}
+			}, 350);
+
+
+
+            d3.event.stopPropagation();
+
+        })
+        .call(drag);
+
+    svg.on('click', function() {
+        if (focusMode && focusedNode) {
+            focusMode = false;
+            unfade(focusedNode, 0);
+        }
+
+    })
+}
+
+function fade(d, opacity) {
+
+    link.style("opacity", function(o) {
+        return isNeighbourLink(d, o) ? 1 : opacity;
+    });
+
+    node.style("opacity", function(o) {
+        return isNeighbour(d, o) ? 1 : opacity;
+    });
+}
+
+function unfade(d, opacity) {
+
+    link.style("opacity", function(o) {
+        return 1;
+    });
+
+    node.style("opacity", function(o) {
+        return 1;
+    });
+}
+
+function isNeighbour(d, o) {
+    if (d.type == "gene2gene") {
+        return d === o || d.genes.indexOf(o) > -1 || d.traits.indexOf(o) > -1 || d.targets.indexOf(o) > -1;
+    } else if (d.type == "gene" || d.type == "trait" || d.type == "target") {
+        if (d.gene2genes.indexOf(o) > -1)
+            return true;
+        for (var i = 0; i < d.gene2genes.length; i++) {
+            if (d.gene2genes[i].genes.indexOf(o) > -1 || d.gene2genes[i].traits.indexOf(o) > -1 || d.gene2genes[i].targets.indexOf(o) > -1)
+                return true;
+        }
+        return d === o;
+    }
+}
+
+function isNeighbourLink(d, o) {
+    if (d.type == "gene2gene")
+        return o.source === d || o.target === d;
+    else if (d.type == "gene" || d.type == "trait" || d.type == "target") {
+        for (var i = 0; i < d.gene2genes.length; i++) {
+            if (o.source === d.gene2genes[i] || o.target === d.gene2genes[i])
+                return true;
+        }
+        return false;
+    }
+}
+
+function tick() {
+    node
+        .filter(function(d) {
+            return d.type == "trait";
+        })
+        .attr("cx", function(d) {
+            return d.x = Math.max(2 * radius, Math.min(5 * radius, d.x));
+        })
+        .attr("cy", function(d) {
+            return d.y = Math.max(5 * radius, Math.min(height - 5 * radius, d.y));
+        });
+    node
+        .filter(function(d) {
+            return d.type == "target";
+        })
+        .attr("cx", function(d) {
+            return d.x = Math.max(width - 5 * radius, Math.min(width - 2 * radius, d.x));
+        })
+        .attr("cy", function(d) {
+            return d.y = Math.max(5 * radius, Math.min(height - 5 * radius, d.y));
+        });
+    node
+        .filter(function(d) {
+            return d.type == "gene" || d.type == "gene2gene";
+        })
+        .attr("cx", function(d) {
+            return d.x = Math.max(radius, Math.min(width - radius, d.x));
+        })
+        .attr("cy", function(d) {
+            return d.y = Math.max(radius, Math.min(height - radius, d.y));
+        });
+
+    link.attr("x1", function(d) {
+            return d.source.x;
+        })
+        .attr("y1", function(d) {
+            return d.source.y;
+        })
+        .attr("x2", function(d) {
+            return d.target.x;
+        })
+        .attr("y2", function(d) {
+            return d.target.y;
+        });
+
+}
+
+function dblclickEvent(d) {
+    d3.select(this).classed("fixed", d.fixed = false);
+}
+
+function dragstart(d) {
+    d3.select(this).classed("fixed", d.fixed = true);
+}
+
+
+var fisheye = d3.fisheye.circular().radius(200);
+
+svg.on("mousemove", function() {
+    fisheye.focus(d3.mouse(this));
+
+    node.each(function(d) {
+        d.fisheye = fisheye(d);
+    });
+    node
+        .attr("cx", function(d) {
+            return d.fisheye.x;
+        })
+        .attr("cy", function(d) {
+            return d.fisheye.y;
+        })
+        .attr("r", function(d) {
+            return Math.pow(3, d.fisheye.z - 1) * d.r;
+        });
+
+    link.attr("x1", function(d) {
+            return d.source.fisheye.x;
+        })
+        .attr("y1", function(d) {
+            return d.source.fisheye.y;
+        })
+        .attr("x2", function(d) {
+            return d.target.fisheye.x;
+        })
+        .attr("y2", function(d) {
+            return d.target.fisheye.y;
+        });
+});
